1c6bd7b5d5a3f97494f427ef01debf08db411b13
Ivan Trombley <itrombley@dot-borg.org>
2020-05-19 11:47:57 -0700

    Rearrange code and add errors

--- src/stats.rs
+++ src/stats.rs
@@ -5,6 +5,11 @@ use gdnative::*;
 #[inherit(Node)]
 pub struct Stats {
   data: LogData,
+  main: NodePath,
+  avatars: NodePath,
+  dates: NodePath,
+  list: NodePath,
+  status: NodePath,
 }
 
 #[methods]
@@ -16,6 +21,11 @@ impl Stats {
     };
     Stats {
       data: LogData::new(folder),
+      main: NodePath::from_str("/root/Main"),
+      avatars: NodePath::from_str("Tools/Avatars"),
+      dates: NodePath::from_str("Tools/Dates"),
+      list: NodePath::from_str("List"),
+      status: NodePath::from_str("Status"),
     }
   }
 
@@ -23,120 +33,118 @@ impl Stats {
   fn _ready(&mut self, owner: Node) {
     unsafe {
       let object = &owner.to_object();
-      if let Some(mut main) = owner.get_main() {
-        let _ = main.connect(
+      if let Some(mut main) = self.get_main(owner) {
+        if let Err(err) = main.connect(
           GodotString::from_str("log_folder_change"),
           Some(*object),
           GodotString::from_str("set_log_folder"),
           VariantArray::new(),
           0,
-        );
+        ) {
+          godot_print!("Unable to connect set_log_folder: {:?}", err);
+        }
       }
 
       let signal = GodotString::from_str("item_selected");
-      if let Some(mut avatars) = owner.get_avatars() {
-        let _ = avatars.connect(
+      if let Some(mut avatars) = self.get_avatars(owner) {
+        if let Err(err) = avatars.connect(
           signal.new_ref(),
           Some(*object),
           GodotString::from_str("avatar_changed"),
           VariantArray::new(),
           0,
-        );
+        ) {
+          godot_print!("Unable to connect avatar_changed: {:?}", err);
+        }
       }
 
-      if let Some(mut dates) = owner.get_dates() {
-        let _ = dates.connect(
+      if let Some(mut dates) = self.get_dates(owner) {
+        if let Err(err) = dates.connect(
           signal.new_ref(),
           Some(*object),
           GodotString::from_str("date_changed"),
           VariantArray::new(),
           0,
-        );
+        ) {
+          godot_print!("Unable to connect date_changed: {:?}", err);
+        }
       }
-      if let Some(mut tree) = owner.get_stats() {
-        tree.set_column_expand(0, true);
-        tree.set_column_min_width(0, 3);
+      if let Some(mut list) = self.get_list(owner) {
+        list.set_column_expand(0, true);
+        list.set_column_min_width(0, 3);
       }
     }
-    owner.populate_avatars(&self.data);
+    self.populate_avatars(owner);
   }
 
   #[export]
   fn avatar_changed(&mut self, owner: Node, item: i64) {
-    if let Some(avatars) = owner.get_avatars() {
+    if let Some(avatars) = self.get_avatars(owner) {
       unsafe {
         let avatar = avatars.get_item_text(item);
         if !avatar.is_empty() {
-          owner.populate_dates(&self.data, Some(avatar.to_utf8().as_str()));
+          self.populate_dates(owner, Some(avatar.to_utf8().as_str()));
           return;
         }
       }
     }
-    owner.populate_dates(&self.data, None);
+    self.populate_dates(owner, None);
   }
 
   #[export]
   fn date_changed(&mut self, owner: Node, item: i64) {
-    if let Some(avatar) = owner.get_current_avatar() {
-      if let Some(dates) = owner.get_dates() {
+    if let Some(avatar) = self.get_current_avatar(owner) {
+      if let Some(dates) = self.get_dates(owner) {
         unsafe {
           let ts = dates.get_item_id(item);
           if ts != 0 {
-            owner.populate_stats(&self.data, Some(avatar.to_utf8().as_str()), Some(ts));
+            self.populate_list(owner, Some(avatar.to_utf8().as_str()), Some(ts));
             return;
           }
         }
       }
     }
-    owner.populate_stats(&self.data, None, None);
+    self.populate_list(owner, None, None);
   }
 
   #[export]
   fn set_log_folder(&mut self, owner: Node, folder: GodotString) {
-    godot_print!("Folder: {}", folder.to_utf8().as_str());
-
     let path = std::path::Path::new(folder.to_utf8().as_str()).to_path_buf();
     self.data = LogData::new(path);
-    owner.populate_avatars(&self.data);
+    self.populate_avatars(owner);
   }
-}
 
-trait StatsNode {
-  fn get_main(&self) -> Option<Node>;
-  fn get_avatars(&self) -> Option<OptionButton>;
-  fn populate_avatars(&self, data: &LogData);
-  fn get_current_avatar(&self) -> Option<GodotString>;
-  fn get_dates(&self) -> Option<OptionButton>;
-  fn populate_dates(&self, data: &LogData, avatar: Option<&str>);
-  fn get_current_date(&self) -> Option<i64>;
-  fn get_stats(&self) -> Option<Tree>;
-  fn populate_stats(&self, data: &LogData, avatar: Option<&str>, ts: Option<i64>);
-  fn get_status(&self) -> Option<Label>;
-  fn set_status_message(&self, text: Option<&str>);
-}
-
-impl StatsNode for Node {
-  fn get_main(&self) -> Option<Node> {
+  fn get_main(&self, owner: Node) -> Option<Node> {
     unsafe {
-      return self.get_node(NodePath::from_str("/root/Main"));
+      let main = owner.get_node(self.main.new_ref());
+      if main.is_none() {
+        godot_print!("Unable to get node Main");
+      }
+      main
     }
   }
 
-  fn get_avatars(&self) -> Option<OptionButton> {
+  fn get_avatars(&self, owner: Node) -> Option<OptionButton> {
     unsafe {
-      if let Some(avatars) = self.get_node(NodePath::from_str("Tools/Avatars")) {
-        return avatars.cast::<OptionButton>();
+      if let Some(avatars) = owner.get_node(self.avatars.new_ref()) {
+        let avatars = avatars.cast::<OptionButton>();
+        if avatars.is_none() {
+          godot_print!("Unable to cast node Avatars as OptionButton");
+        }
+        return avatars;
+      } else {
+        godot_print!("Unable to get node Avatars");
       }
     }
     None
   }
 
-  fn populate_avatars(&self, data: &LogData) {
-    if let Some(mut avatars) = self.get_avatars() {
+  fn populate_avatars(&self, owner: Node) {
+    if let Some(mut avatars) = self.get_avatars(owner) {
       unsafe {
         avatars.clear();
 
-        let names = data.get_avatars();
+        let names = self.data.get_avatars();
         for (idx, name) in names.iter().enumerate() {
           avatars.add_item(GodotString::from_str(&name), idx as i64 + 1);
         }
@@ -144,18 +152,18 @@ impl StatsNode for Node {
         if avatars.get_item_count() > 0 {
           let avatar = avatars.get_item_text(0);
           if !avatar.is_empty() {
-            self.populate_dates(data, Some(avatar.to_utf8().as_str()));
+            self.populate_dates(owner, Some(avatar.to_utf8().as_str()));
             return;
           }
         }
       }
     }
-    self.populate_dates(data, None);
+    self.populate_dates(owner, None);
   }
 
-  fn get_current_avatar(&self) -> Option<GodotString> {
+  fn get_current_avatar(&self, owner: Node) -> Option<GodotString> {
     unsafe {
-      if let Some(avatars) = self.get_avatars() {
+      if let Some(avatars) = self.get_avatars(owner) {
         let id = avatars.get_selected_id();
         if id != 0 {
           let avatar = avatars.get_item_text(avatars.get_item_index(id));
@@ -168,21 +176,27 @@ impl StatsNode for Node {
     None
   }
 
-  fn get_dates(&self) -> Option<OptionButton> {
+  fn get_dates(&self, owner: Node) -> Option<OptionButton> {
     unsafe {
-      if let Some(dates) = self.get_node(NodePath::from_str("Tools/Dates")) {
-        return dates.cast::<OptionButton>();
+      if let Some(dates) = owner.get_node(self.dates.new_ref()) {
+        let dates = dates.cast::<OptionButton>();
+        if dates.is_none() {
+          godot_print!("Unable to cast node Dates as OptionButton");
+        }
+        return dates;
+      } else {
+        godot_print!("Unable to get node Dates");
       }
     }
     None
   }
 
-  fn populate_dates(&self, data: &LogData, avatar: Option<&str>) {
-    if let Some(mut dates) = self.get_dates() {
+  fn populate_dates(&self, owner: Node, avatar: Option<&str>) {
+    if let Some(mut dates) = self.get_dates(owner) {
       unsafe {
         dates.clear();
         if let Some(avatar) = avatar {
-          let timestamps = data.get_stats_timestamps(avatar);
+          let timestamps = self.data.get_stats_timestamps(avatar);
           if !timestamps.is_empty() {
             for ts in timestamps {
               let date = timestamp_to_view_date(ts);
@@ -191,19 +205,19 @@ impl StatsNode for Node {
 
             let ts = dates.get_item_id(0);
             if ts != 0 {
-              self.populate_stats(data, Some(avatar), Some(ts));
+              self.populate_list(owner, Some(avatar), Some(ts));
               return;
             }
           }
         }
       }
     }
-    self.populate_stats(data, avatar, None);
+    self.populate_list(owner, avatar, None);
   }
 
-  fn get_current_date(&self) -> Option<i64> {
+  fn _get_current_date(&self, owner: Node) -> Option<i64> {
     unsafe {
-      if let Some(dates) = self.get_dates() {
+      if let Some(dates) = self.get_dates(owner) {
         let ts = dates.get_selected_id();
         if ts != 0 {
           return Some(ts);
@@ -213,31 +227,37 @@ impl StatsNode for Node {
     None
   }
 
-  fn get_stats(&self) -> Option<Tree> {
+  fn get_list(&self, owner: Node) -> Option<Tree> {
     unsafe {
-      if let Some(stats) = self.get_node(NodePath::from_str("Tree")) {
-        return stats.cast::<Tree>();
+      if let Some(list) = owner.get_node(self.list.new_ref()) {
+        let list = list.cast::<Tree>();
+        if list.is_none() {
+          godot_print!("Unable to cast node List as Tree");
+        }
+        return list;
+      } else {
+        godot_print!("Unable to get node List");
       }
     }
     None
   }
 
-  fn populate_stats(&self, data: &LogData, avatar: Option<&str>, ts: Option<i64>) {
-    self.set_status_message(None);
+  fn populate_list(&self, owner: Node, avatar: Option<&str>, ts: Option<i64>) {
+    self.set_status_message(owner, None);
     unsafe {
-      let mut tree = some!(self.get_stats());
-      tree.clear();
+      let mut list = some!(self.get_list(owner));
+      list.clear();
 
       let avatar = some!(avatar);
       let ts = some!(ts);
 
-      if let Some(stats) = data.get_stats(avatar, ts) {
-        if let Some(parent) = tree.create_item(None, -1) {
+      if let Some(stats) = self.data.get_stats(avatar, ts) {
+        if let Some(parent) = list.create_item(None, -1) {
           let locale = get_locale();
           let mut alt_bg = false;
           for (name, value) in stats.iter() {
             if let Ok(num) = value.replacen(',', ".", 1).parse::<f64>() {
-              if let Some(mut item) = tree.create_item(parent.cast::<Object>(), -1) {
+              if let Some(mut item) = list.create_item(parent.cast::<Object>(), -1) {
                 let value = &num.to_display_string(&locale);
                 let tip = GodotString::from_str(&format!("{} = {}", name, value));
                 let bg = if alt_bg {
@@ -262,27 +282,33 @@ impl StatsNode for Node {
           }
         }
         let text = format!("Showing stats from {}", timestamp_to_view_date(ts));
-        self.set_status_message(Some(&text));
+        self.set_status_message(owner, Some(&text));
         return;
       }
       let text = format!("No stats found for {}", avatar);
-      self.set_status_message(Some(&text));
+      self.set_status_message(owner, Some(&text));
       return;
     }
   }
 
-  fn get_status(&self) -> Option<Label> {
+  fn get_status(&self, owner: Node) -> Option<Label> {
     unsafe {
-      if let Some(status) = self.get_node(NodePath::from_str("Status")) {
-        return status.cast::<Label>();
+      if let Some(status) = owner.get_node(self.status.new_ref()) {
+        let status = status.cast::<Label>();
+        if status.is_none() {
+          godot_print!("Unable to cast node Status as Label");
+        }
+        return status;
+      } else {
+        godot_print!("Unable to get node Status");
       }
     }
     None
   }
 
-  fn set_status_message(&self, text: Option<&str>) {
+  fn set_status_message(&self, owner: Node, text: Option<&str>) {
     unsafe {
-      if let Some(mut status) = self.get_status() {
+      if let Some(mut status) = self.get_status(owner) {
         match text {
           Some(text) => status.set_text(GodotString::from_str(text)),
           None => status.set_text(GodotString::new()),
