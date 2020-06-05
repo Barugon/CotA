1340383d79a25f85f1168b248b8ee3a3d909cd66
Ivan Trombley <itrombley@dot-borg.org>
2020-06-04 20:03:11 -0700

    Add Offline tab

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,13 @@ mod util;
 mod app;
 mod experience;
 mod menu;
+mod offline;
 mod portals;
 mod stats;
 
 use crate::app::*;
 use crate::experience::*;
+use crate::offline::*;
 use crate::portals::*;
 use crate::stats::*;
 use gdnative::*;
@@ -16,6 +18,7 @@ use gdnative::*;
 fn init(handle: gdnative::init::InitHandle) {
   handle.add_class::<App>();
   handle.add_class::<Experience>();
+  handle.add_class::<Offline>();
   handle.add_class::<Portals>();
   handle.add_class::<Stats>();
 }
