d985a20906918f5a9c9b98f9cc593ea5751071c5
Ivan Trombley <itrombley@dot-borg.org>
2020-06-06 13:45:49 -0700

    More work on offline tab

--- src/offline.rs
+++ src/offline.rs
@@ -1,6 +1,7 @@
 use crate::constants::*;
 use crate::util::*;
 use gdnative::*;
+use std::{fs::File, io::prelude::*, path::Path};
 use xml_dom::*;
 
 enum SkillTree {
@@ -13,20 +14,22 @@ struct GameInfo {
   path: GodotString,
 }
 
-fn get_node_json(node: &level2::RefNode, name: &str) -> Option<String> {
-  if let Ok(document) = level2::convert::as_document(node) {
-    let collections = document.get_elements_by_tag_name("collection");
-    for collection in collections {
-      if let Ok(element) = level2::convert::as_element(&collection) {
-        if let Some(attribute) = element.get_attribute("name") {
-          if attribute == name {
-            let records = element.get_elements_by_tag_name("record");
-            for record in records {
-              if let Ok(element) = level2::convert::as_element(&record) {
-                let nodes = element.child_nodes();
-                for node in nodes {
-                  if let Ok(text) = level2::convert::as_text(&node) {
-                    return text.node_value();
+impl GameInfo {
+  fn get_node_json(&self, name: &str) -> Option<String> {
+    if let Ok(document) = level2::convert::as_document(&self.node) {
+      let collections = document.get_elements_by_tag_name("collection");
+      for collection in collections {
+        if let Ok(element) = level2::convert::as_element(&collection) {
+          if let Some(attribute) = element.get_attribute("name") {
+            if attribute == name {
+              let records = element.get_elements_by_tag_name("record");
+              for record in records {
+                if let Ok(element) = level2::convert::as_element(&record) {
+                  let nodes = element.child_nodes();
+                  for node in nodes {
+                    if let Ok(text) = level2::convert::as_text(&node) {
+                      return text.node_value();
+                    }
                   }
                 }
               }
@@ -35,8 +38,67 @@ fn get_node_json(node: &level2::RefNode, name: &str) -> Option<String> {
         }
       }
     }
+    None
   }
-  None
+
+  fn set_node_json(&mut self, name: &str, json: &str) -> bool {
+    if let Ok(document) = level2::convert::as_document_mut(&mut self.node) {
+      let mut collections = document.get_elements_by_tag_name("collection");
+      for collection in &mut collections {
+        if let Ok(element) = level2::convert::as_element_mut(collection) {
+          if let Some(attribute) = element.get_attribute("name") {
+            if attribute == name {
+              let mut records = element.get_elements_by_tag_name("record");
+              for record in &mut records {
+                if let Ok(element) = level2::convert::as_element_mut(record) {
+                  let mut nodes = element.child_nodes();
+                  for node in &mut nodes {
+                    if let Ok(text) = level2::convert::as_text_mut(node) {
+                      return text.set_node_value(json).is_ok();
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    false
+  }
+}
+
+struct CharacterInfo {
+  char_json: json::JsonValue,
+  gold_json: json::JsonValue,
+  date: String,
+}
+
+impl CharacterInfo {
+  fn new(info: &Option<GameInfo>) -> Option<Self> {
+    if let Some(info) = info {
+      // Get the 'CharacterSheet' json.
+      if let Some(text) = info.get_node_json("CharacterSheet") {
+        if let Ok(char_json) = json::parse(&text) {
+          // Get the date.
+          if let Some(date) = char_json["rd"]["c"].as_str() {
+            // Ge the 'UserGold' json.
+            if let Some(text) = info.get_node_json("UserGold") {
+              if let Ok(gold_json) = json::parse(&text) {
+                let date = String::from(date);
+                return Some(Self {
+                  char_json,
+                  gold_json,
+                  date,
+                });
+              }
+            }
+          }
+        }
+      }
+    }
+    None
+  }
 }
 
 #[derive(NativeClass)]
@@ -51,6 +113,7 @@ pub struct Offline {
   file_dialog: NodePath,
   file_dialog_title: GodotString,
   file_filters: StringArray,
+  status: NodePath,
 }
 
 #[methods]
@@ -68,6 +131,7 @@ impl Offline {
       file_dialog: NodePath::from_str("/root/App/FileDialog"),
       file_dialog_title: GodotString::from_str("Select Saved Game"),
       file_filters: filters,
+      status: NodePath::from_str("Label"),
     }
   }
 
@@ -84,22 +148,16 @@ impl Offline {
 
     // Connect save_clicked.
     owner.connect_to(&self.save, "pressed", "save_clicked");
-
-    // TODO: do this on load.
-    if let Some(mut gold) = owner.get_node_as::<SpinBox>(&self.gold) {
-      unsafe {
-        gold.set_editable(true);
-        gold.set_focus_mode(Control::FOCUS_ALL);
-      }
-    }
   }
 
   #[export]
   fn item_changed(&self, owner: Node) {
-    if let Some(mut button) = owner.get_node_as::<Button>(&self.save) {
-      unsafe {
-        button.set_disabled(false);
-        button.set_focus_mode(Control::FOCUS_ALL);
+    if self.info.is_some() {
+      if let Some(mut button) = owner.get_node_as::<Button>(&self.save) {
+        unsafe {
+          button.set_disabled(false);
+          button.set_focus_mode(Control::FOCUS_ALL);
+        }
       }
     }
   }
@@ -124,37 +182,89 @@ impl Offline {
 
   #[export]
   fn file_selected(&mut self, owner: Node, path: GodotString) {
-    if let Ok(text) = std::fs::read_to_string(path.to_utf8().as_str()) {
+    let utf8 = path.to_utf8();
+    let path_str = utf8.as_str();
+    if let Ok(text) = std::fs::read_to_string(path_str) {
       if let Ok(node) = parser::read_xml(&text) {
-        if let Some(text) = get_node_json(&node, "CharacterSheet") {
-          if let Ok(json) = json::parse(&text) {
-            let json = &json["sk2"];
-            if json.is_object() {
-              if self.populate_tree(owner, &self.adventurer, json)
-                && self.populate_tree(owner, &self.producer, json)
-              {
-                self.info = Some(GameInfo { node, path });
+        let game_info = Some(GameInfo {
+          node,
+          path: path.new_ref(),
+        });
+        if let Some(char_info) = CharacterInfo::new(&game_info) {
+          let json = &char_info.char_json["sk2"];
+          if json.is_object() {
+            if self.populate_tree(owner, &self.adventurer, json)
+              && self.populate_tree(owner, &self.producer, json)
+            {
+              if let Some(path) = Path::new(path_str).file_name() {
+                if let Some(path) = path.to_str() {
+                  self.set_status_message(owner, &format!("Editing '{}'", path));
+                }
+              }
+              self.info = game_info;
+              return;
+            }
+          }
+        }
+      }
+    }
+    // Failure to edit. Cleanup
+    self.disable_tree(owner, &self.adventurer);
+    self.disable_tree(owner, &self.producer);
+
+    if let Some(mut button) = owner.get_node_as::<Button>(&self.save) {
+      unsafe {
+        button.set_disabled(true);
+        button.set_focus_mode(Control::FOCUS_NONE);
+      }
+    }
+
+    if let Some(mut gold) = owner.get_node_as::<SpinBox>(&self.gold) {
+      unsafe {
+        gold.set_value(0.0);
+        gold.set_editable(false);
+        gold.set_focus_mode(Control::FOCUS_NONE);
+      }
+    }
+
+    if let Some(path) = Path::new(path_str).file_name() {
+      if let Some(path) = path.to_str() {
+        self.set_status_message(owner, &format!("Unable to edit '{}'", path));
+      }
+    }
+  }
+
+  #[export]
+  fn save_clicked(&mut self, owner: Node) {
+    if let Some(mut char_info) = CharacterInfo::new(&self.info) {
+      let sk2 = &mut char_info.char_json["sk2"];
+      if sk2.is_object() {
+        if self.collect_skills(owner, &self.adventurer, sk2, &char_info.date)
+          && self.collect_skills(owner, &self.producer, sk2, &char_info.date)
+        {
+          if let Some(info) = &mut self.info {
+            if info.set_node_json("CharacterSheet", &char_info.char_json.to_string()) {
+              if let Ok(mut file) = File::create(info.path.to_utf8().as_str()) {
+                if file.write_all(info.node.to_string().as_bytes()).is_ok() {
+                  if let Some(mut button) = owner.get_node_as::<Button>(&self.save) {
+                    unsafe {
+                      button.set_disabled(true);
+                      button.set_focus_mode(Control::FOCUS_NONE);
+                    }
+                  }
+                  return;
+                }
               }
             }
           }
         }
       }
     }
-  }
 
-  #[export]
-  fn save_clicked(&self, _owner: Node) {
     if let Some(info) = &self.info {
-      if let Some(text) = get_node_json(&info.node, "CharacterSheet") {
-        if let Ok(json) = json::parse(&text) {
-          let json = &json["sk2"];
-          if json.is_object() {
-            godot_print!("gak");
-            // if self.populate_tree(owner, &self.adventurer, json)
-            // && self.populate_tree(owner, &self.producer, json)
-            // {
-            // }
-          }
+      if let Some(path) = Path::new(info.path.to_utf8().as_str()).file_name() {
+        if let Some(path) = path.to_str() {
+          self.set_status_message(owner, &format!("Unable to save '{}'", path));
         }
       }
     }
@@ -169,6 +279,17 @@ impl Offline {
       "item_edited",
       "item_changed",
     );
+  }
+
+  fn disable_tree(&self, owner: Node, tree: &SkillTree) {
+    let mut tree = match tree {
+      SkillTree::Adventurer(path) => some!(owner.get_node_as::<Tree>(path)),
+      SkillTree::Producer(path) => some!(owner.get_node_as::<Tree>(path)),
+    };
+    unsafe {
+      tree.clear();
+      tree.set_focus_mode(Control::FOCUS_NONE);
+    }
   }
 
   fn populate_tree(&self, owner: Node, tree: &SkillTree, json: &json::JsonValue) -> bool {
@@ -207,6 +328,12 @@ impl Offline {
             continue;
           };
 
+          let mul_val = if let Ok(val) = mul.parse::<f64>() {
+            val
+          } else {
+            continue;
+          };
+
           let id = if let Some(text) = iter.next() {
             if text.parse::<u32>().is_err() {
               continue;
@@ -229,10 +356,10 @@ impl Offline {
             item.set_custom_color(2, info_color);
             item.set_text(2, GodotString::from_str(id));
 
-            let level = if let Some(val) = json[id]["x"].as_i64() {
+            let level = if let Some(val) = json[id]["x"].as_f64() {
               let mut level = 0;
               for (lvl, exp) in EXP_VALUES.iter().enumerate().rev() {
-                if val >= *exp {
+                if val >= *exp as f64 * mul_val {
                   level = lvl + 1;
                   break;
                 }
@@ -255,5 +382,65 @@ impl Offline {
     false
   }
 
-  // fn collect_skills(&self, owner: Node, tree: &SkillTree, json: &json::JsonValue) {}
+  fn collect_skills(
+    &self,
+    owner: Node,
+    tree: &SkillTree,
+    json: &mut json::JsonValue,
+    date: &str,
+  ) -> bool {
+    let mut tree = match tree {
+      SkillTree::Adventurer(path) => some!(owner.get_node_as::<Tree>(path), false),
+      SkillTree::Producer(path) => some!(owner.get_node_as::<Tree>(path), false),
+    };
+    unsafe {
+      let mut root = some!(tree.get_root(), false);
+      let mut node = root.get_children();
+      loop {
+        if let Some(mut item) = node {
+          if let Ok(mul) = item
+            .get_text(1)
+            .to_utf8()
+            .as_str()
+            .trim_end_matches('x')
+            .parse::<f64>()
+          {
+            let utf8 = item.get_text(2).to_utf8();
+            let key = utf8.as_str();
+            let lvl = item.get_range(3) as usize;
+            if lvl > 0 {
+              let exp = (EXP_VALUES[lvl - 1] as f64 * mul).ceil() as i64;
+              if let Some(cur) = json[key]["x"].as_i64() {
+                // Change it only if it's different.
+                if exp != cur {
+                  json[key]["x"] = json::JsonValue::from(exp);
+                }
+              } else {
+                // Add a new object for the skill ID.
+                json[key] = json::object! {
+                  "x": exp,
+                  "t": date,
+                  "m": 0
+                };
+              }
+            } else {
+              // Remove the skill if it exists.
+              json.remove(key);
+            }
+          }
+          node = item.get_next();
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+
+  fn set_status_message(&self, owner: Node, text: &str) {
+    if let Some(mut label) = owner.get_node_as::<Label>(&self.status) {
+      unsafe {
+        label.set_text(GodotString::from_str(text));
+      }
+    }
+  }
 }
